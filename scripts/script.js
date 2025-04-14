eaeb084891fd3e1b174391dba3c5ebfc60dc5c71
Renato Tenório Lino <renatotl@outlook.com>
2025-04-14 00:23:19 -0300

    update

--- scripts/script.js
+++ scripts/script.js
@@ -7,19 +7,19 @@ elementos.forEach(elemento => {
         const id = elemento.id;
         console.log("foi");
         if(id== "quadrado2"){
-            window.location.replace("src/cozinha.html");
+            window.location.replace("views/cozinha.html");
              console.log(" cozinha funcionol");
         } else if(id=="quadrado3"){
-            window.location.replace("src/quarto.html");
+            window.location.replace("views/quarto.html");
             console.log("quarto funviona")
         } else if(id=="quadrado4"){
-            window.location.replace("src/quintal.html") ;
+            window.location.replace("views/quintal.html") ;
             console.log("quintal funviona") ;
         } else if(id=="quadrado5"){
-            window.location.replace("src/sala.html");
+            window.location.replace("views/sala.html");
             console.log("sala funciona");
         }else if(id=="quadrado1"){
-            window.location.replace("src/banheiro.html");
+            window.location.replace("views/banheiro.html");
             console.log("banheiro funciona")
         };
     });
